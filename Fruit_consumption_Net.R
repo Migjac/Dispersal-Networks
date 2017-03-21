2bc72403799fbc9fed29449f0412e1e5d4b66915
Migjac <mjacome@ebd.csic.es>
2017-03-21 10:13:17 +0100

    Updated scripts

--- Fruit_consumption_Net.R
+++ Fruit_consumption_Net.R
@@ -1,4 +1,4 @@
-##*Fruit remotion rates*
+##*Fruit removal rates*
 #**Step 1. **Open Martinazo and Matasgordas matrix
 
 #Opening a matrix file with weighted links with frugivores fruit remotion in individual plants. Other specifications of the matrix are described in the previous section.
@@ -26,17 +26,19 @@ plotweb(matas_f, col.low=c(gray(seq(0.1,1,length=39))), col.high=c("white","whit
 
 #Centrality measures
 
-#Degree
-specieslevel(mart_f,index="normalised degree")
-specieslevel(matas_f,index="normalised degree")
+#Degree, Betweeness and Closeness
 
-#Closeness
-specieslevel(mart_f,index="closeness")
-specieslevel(matas_f,index="closeness")
+HLspplvlmartf<-specieslevel(mart_f)
+HLspplvlmartf$`higher level`[,c(2,11,12,13,14)]
+LLspplvlmartf<-specieslevel(mart_f)
+LLspplvlmartf$`lower level`[,c(2,11,12,13,14)]
 
-#Betweeness
-specieslevel(mart_f,index="betweenness")
-specieslevel(matas_f,index="betweenness")
+spplvlmatasf<-specieslevel(matas_f)
+spplvlmatasf$`higher level`[,c(2,11,12,13,14)]
+LLspplvlmatasf<-specieslevel(matas_f)
+LLspplvlmatasf$`lower level`[,c(2,11,12,13,14)]
+
+
 
 
 ##Networks analysis. Network topology indexes
@@ -66,6 +68,16 @@ par(mfrow=c(1,1))
 mart_matas_fruits<-read.csv("~/Desktop/Palmito/Postdoc-Interaction Networks/Mart_matas_fruits.csv",header=TRUE,check.names=FALSE,row.names=1)
 plotweb(mart_matas_fruits, col.low=c(gray(seq(0.1,1,length=56))), col.high=c("white","white","gray30","gray30","black","black","darkgray"),method="normal", text.rot=90,low.lablength=10, high.lablength=8)
 
+#Centrality measures
+
+#Degree, Betweeness and Closeness
+
+HLspplvlmartf<-specieslevel(mart_matas_fruits)
+HLspplvlmartf$`higher level`[,c(2,11,12,13,14)]
+LLspplvlmartf<-specieslevel(mart_matas_fruits)
+LLspplvlmartf$`lower level`[,c(2,11,12,13,14)]
+
+
 ##**Step 5.** Changing the matrix by eliminating unknown visitors
 
 
@@ -83,4 +95,227 @@ plotweb(mart_fnun, col.low=c(gray(seq(0.1,1,length=22))), col.high=c("white","wh
 
 #Matasgordas Bipartite Network
 plotweb(matas_fnun, col.low=c(gray(seq(0.1,1,length=30))), col.high=c("white","white","gray30","gray30","black","black","darkgray"),method="normal", text.rot=90,low.lablength=10, high.lablength=8)
-```
+
+
+#Do the centrality is related to the spatial location of individual in a population?
+#Centrality related to aggregation and density 
+
+##GAM
+par(mfrow=c(3,3))
+library(ISLR)
+#Martinazo
+mart_wf
+
+#Estimate Std. Error t value Pr(>|t|)   
+#(Intercept)  0.30200    0.08626   3.501  0.00257 **
+#  dens         0.00321    0.02947   0.109  0.91448   
+#  edf Ref.df     F p-value
+#s(nn)       1.000  1.000 0.217   0.647
+#s(del.area) 1.115  1.219 1.254   0.338
+#R-sq.(adj) =  -0.0692   Deviance explained = 8.94%
+mart_fnd<-gam(nd~s(nn)+s(del.area)+dens,data = mart_wf)
+plot(mart_fnd,residuals=TRUE, se=TRUE, shade=TRUE,all.terms = TRUE, main = "Normalised degree")
+summary(mart_fnd)
+
+#Estimate Std. Error t value Pr(>|t|)  
+#(Intercept)  0.060454   0.019164   3.155   0.0168 *
+# dens        -0.005593   0.006813  -0.821   0.4397  
+#  edf Ref.df    F  p-value    
+#s(nn)       8.900  8.988 37.3 1.18e-10 ***
+# s(del.area) 4.342  5.076 47.8 3.87e-09 ***
+#R-sq.(adj) =  0.959   Deviance explained = 98.7%
+#GCV = 0.0023964  Scale est. = 0.00073607  n = 22
+mart_fwb<-gam(wbtw~s(nn)+s(del.area)+dens,data = mart_wf)
+plot(mart_fwb,residuals= TRUE, se=TRUE, shade=TRUE,all.terms = TRUE, main = "Weighted Betweeness")
+summary(mart_fwb)
+
+#Estimate Std. Error t value Pr(>|t|)    
+#(Intercept) 0.029254   0.006872   4.257 0.000474 ***
+#  dens        0.002356   0.002345   1.005 0.328289    
+#  edf Ref.df     F p-value
+#s(nn)         1      1 0.678   0.421
+#s(del.area)   1      1 0.090   0.767
+#R-sq.(adj) =  -0.0966   Deviance explained = 6.01%
+#GCV = 0.00020691  Scale est. = 0.00016929  n = 22
+mart_fwc<-gam(wcls~s(nn)+s(del.area)+dens,data = mart_wf)
+plot(mart_fwc,residuals= TRUE, se=TRUE, shade=TRUE,all.terms = TRUE, main = "Weighted Closeness")
+summary(mart_fwc)
+
+#Matasgordas
+matas_wf<-read.csv("~/Desktop/Palmito/Postdoc-Interaction Networks/matas_wf.csv",header=TRUE)
+matas_wf
+
+#Estimate Std. Error t value Pr(>|t|)    
+#(Intercept)  0.332488   0.050840   6.540 3.12e-07 ***
+#  dens        -0.006715   0.008677  -0.774    0.445    
+#  edf Ref.df     F p-value
+#s(nn)         1      1 0.008   0.928
+#s(del.area)   1      1 0.000   0.998
+#R-sq.(adj) =  -0.067   Deviance explained =    3%
+#GCV = 0.02448  Scale est. = 0.0216    n = 34
+matas_fnd<-gam(nd~s(nn)+s(del.area)+dens,data = matas_wf)
+plot(matas_fnd,residuals=TRUE, se=TRUE, shade=TRUE,all.terms = TRUE, main = "Normalised Degree")
+summary(matas_fnd)
+
+#Estimate Std. Error t value Pr(>|t|)
+#(Intercept) 0.0278111  0.0195036   1.426    0.164
+#dens        0.0003146  0.0033288   0.095    0.925
+# edf Ref.df     F p-value
+#s(nn)         1      1 0.343   0.562
+#s(del.area)   1      1 0.231   0.634
+#R-sq.(adj) =  -0.0611   Deviance explained = 3.53%
+#GCV = 0.0036027  Scale est. = 0.0031789  n = 34
+matas_fwb<-gam(wbtw~s(nn)+s(del.area)+dens,data = matas_wf)
+plot(matas_fwb,residuals= TRUE, se=TRUE, shade=TRUE,all.terms = TRUE, main = "Weighted Betweeness")
+summary(matas_fwb)
+
+#Estimate Std. Error t value Pr(>|t|)    
+#(Intercept) 0.0225772  0.0028818   7.834 9.64e-09 ***
+#  dens        0.0005464  0.0004919   1.111    0.275    
+#  Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+#  edf Ref.df     F p-value
+#s(nn)         1      1 0.168   0.684
+#s(del.area)   1      1 0.620   0.437
+#R-sq.(adj) =  -0.046   Deviance explained = 4.91%
+#GCV = 7.8658e-05  Scale est. = 6.9404e-05  n = 34
+matas_fwc<-gam(wcls~s(nn)+s(del.area)+dens,data = matas_wf)
+plot(matas_fwc,residuals= TRUE, se=TRUE, shade=TRUE,all.terms = TRUE, main = "Weighted Closeness")
+summary(matas_fwc)
+
+
+#Fulldata using the data of the matrixs analysed separetly
+mart_matas_wf<-read.csv("~/Desktop/Palmito/Postdoc-Interaction Networks/mart_matas_wf.csv",header=TRUE)
+mart_matas_wf
+
+#Estimate Std. Error t value Pr(>|t|)    
+#(Intercept)  0.329839   0.036682   8.992 3.57e-12 ***
+#  dens        -0.006443   0.007426  -0.868     0.39    
+#  edf Ref.df    F p-value
+#s(nn)         1      1 0.01   0.922
+#s(del.area)   1      1 0.40   0.530
+#R-sq.(adj) =  -0.0363   Deviance explained = 2.03%
+#GCV = 0.024075  Scale est. = 0.022355  n = 56
+mart_matas_fnd<-gam(nd~s(nn)+s(del.area)+dens,data = mart_matas_wf)
+plot(mart_matas_fnd,residuals=TRUE, se=TRUE, shade=TRUE, all.terms = TRUE, main = "Normalised Degree")
+summary(mart_matas_fnd)
+
+#Estimate Std. Error t value Pr(>|t|)
+#(Intercept)  0.0378415  0.0237189   1.595    0.117
+#dens        -0.0005135  0.0050278  -0.102    0.919
+# edf Ref.df     F p-value  
+#s(nn)       5.638  6.791 2.078  0.0647 .
+#s(del.area) 1.000  1.000 2.280  0.1376  
+#R-sq.(adj) =   0.17   Deviance explained = 28.5%
+#GCV = 0.0085227  Scale est. = 0.007208  n = 56
+mart_matas_fwb<-gam(wbtw~s(nn)+s(del.area)+dens,data = mart_matas_wf)
+plot(mart_matas_fwb,residuals= TRUE, se=TRUE, shade=TRUE,all.terms = TRUE, main = "Weighted Betweeness")
+summary(mart_matas_fwb)
+
+#Estimate Std. Error t value Pr(>|t|)    
+#(Intercept) 0.0282913  0.0027622  10.242 4.52e-14 ***
+#  dens        0.0002605  0.0005592   0.466    0.643    
+#  edf Ref.df     F p-value
+#s(nn)         1      1 1.614   0.210
+#s(del.area)   1      1 0.034   0.855
+#R-sq.(adj) =  -0.0214   Deviance explained = 3.43%
+#GCV = 0.00013651  Scale est. = 0.00012676  n = 56
+mart_matas_fwc<-gam(wcls~s(nn)+s(del.area)+dens,data = mart_matas_wf)
+plot(mart_matas_fwc,residuals= TRUE, se=TRUE, shade=TRUE, all.terms=TRUE, main = "Weighted Closeness")
+summary(mart_matas_fwc)
+
+#With a subset excluding all palms far away from 20 m. A reduce in the significance an the explained variance
+prb<-mart_matas_wf[1:48,]
+
+#Estimate Std. Error t value Pr(>|t|)
+#(Intercept) 0.0392092  0.0262428   1.494    0.144
+#dens        0.0001987  0.0053611   0.037    0.971
+#  edf Ref.df     F p-value   
+#s(nn)       8.427  8.901 3.714 0.00144 **
+#  s(del.area) 1.000  1.000 0.257 0.61522   
+#R-sq.(adj) =   0.38   Deviance explained = 51.8%
+#GCV = 0.0080653  Scale est. = 0.0061452  n = 48
+mart_matas_fwb1<-gam(wbtw~s(nn)+s(del.area)+dens,data = prb)
+plot(mart_matas_fwb1,residuals= TRUE, se=TRUE, shade=TRUE,all.terms = TRUE, main = "Weighted Betweeness")
+summary(mart_matas_fwb1)
+
+#Estimate Std. Error t value Pr(>|t|)    
+#(Intercept) 0.0265263  0.0029441   9.010  1.5e-11 ***
+#  dens        0.0004750  0.0005661   0.839    0.406    
+#  edf Ref.df     F p-value
+#s(nn)         1      1 2.266   0.139
+#s(del.area)   1      1 0.194   0.662
+#R-sq.(adj) =  -0.0127   Deviance explained = 5.19%
+#GCV = 0.00012651  Scale est. = 0.00011596  n = 48
+mart_matas_fwc1<-gam(wcls~s(nn)+s(del.area)+dens,data = prb)
+plot(mart_matas_fwc1,residuals= TRUE, se=TRUE, shade=TRUE, all.terms=TRUE, main = "Weighted Closeness")
+summary(mart_matas_fwc1)
+
+
+
+#Using the data of the matrix analysed together
+mart_matas_wf2<-read.csv("~/Desktop/Palmito/Postdoc-Interaction Networks/Dispersal-Networks/MartmatasLL_wf.csv",header=TRUE)
+mart_matas_wf2
+
+#Estimate Std. Error t value Pr(>|t|)    
+#(Intercept)  0.305141   0.034871   8.751 8.46e-12 ***
+#  dens        -0.004689   0.007059  -0.664    0.509    
+#  edf Ref.df     F p-value
+#s(nn)         1      1 0.089   0.767
+#s(del.area)   1      1 0.191   0.664
+#R-sq.(adj) =  -0.0471   Deviance explained = 1.01%
+#GCV = 0.021757  Scale est. = 0.020203  n = 56
+mart_matas_fnd2<-gam(nd~s(nn)+s(del.area)+dens,data = mart_matas_wf2)
+plot(mart_matas_fnd2,residuals=TRUE, se=TRUE, shade=TRUE, all.terms = TRUE, main = "Normalised Degree")
+summary(mart_matas_fnd2)
+
+#Estimate Std. Error t value Pr(>|t|)
+#(Intercept) 0.0143885  0.0092280   1.559    0.125
+#dens        0.0008373  0.0018680   0.448    0.656
+# edf Ref.df     F p-value
+#s(nn)         1      1 0.002   0.961
+#s(del.area)   1      1 0.682   0.413
+#R-sq.(adj) =  -0.0285   Deviance explained = 2.76%
+#GCV = 0.0015236  Scale est. = 0.0014148  n = 56
+mart_matas_fwb2<-gam(wbtw~s(nn)+s(del.area)+dens,data = mart_matas_wf2)
+plot(mart_matas_fwb2,residuals= TRUE, se=TRUE, shade=TRUE,all.terms = TRUE, main = "Weighted Betweeness")
+summary(mart_matas_fwb2)
+
+#Estimate Std. Error t value Pr(>|t|)    
+#(Intercept) 0.0139563  0.0011472  12.165   <2e-16 ***
+#  dens        0.0001236  0.0002322   0.532    0.597    
+#  edf Ref.df     F p-value
+#s(nn)         1      1 0.178   0.675
+#s(del.area)   1      1 0.965   0.331
+#R-sq.(adj) =  -0.0335   Deviance explained = 2.29%
+#GCV = 2.3548e-05  Scale est. = 2.1866e-05  n = 56
+mart_matas_fwc2<-gam(wcls~s(nn)+s(del.area)+dens,data = mart_matas_wf2)
+plot(mart_matas_fwc2,residuals= TRUE, se=TRUE, shade=TRUE, all.terms=TRUE, main = "Weighted Closeness")
+summary(mart_matas_fwc2)
+
+
+#Interaction motifs
+
+#Obtaining Median of non-zero interactions. 
+
+martmat_fNA<-mart_matas_fruits
+
+martmat_fNA<-mart_matas_fruits
+martmat_fNA[martmat_fNA==0]<-NA
+martmat_fNA
+
+##Median excluding NAs(e.g. 0). "The median for proportional visits is (0.1667)". All datas above this value will mean strong interaction and all below weak interaction
+median(as.matrix(martmat_fNA),na.rm=TRUE)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Visit_Interaction_Net.R
+++ Visit_Interaction_Net.R
@@ -2,6 +2,19 @@
 
   ##*Frugivores visits*
 
+
+####Calculate aggregation degree of given plants via neares neighbour function in "spatstat package"
+
+attach(martcoord)
+nnmart<-nndist(martcoord$x, martcoord$y)
+write.table(nnmart, "/Users/apple/Desktop/Palmito/Postdoc-Interaction Networks/Dispersal-Networks/nnmart.txt", sep="\t")
+detach(martcoord)
+
+attach(matascoord)
+nnmatas<-nndist(matascoord$x_proj, matascoord$y_proj)
+write.table(nnmatas, "/Users/apple/Desktop/Palmito/Postdoc-Interaction Networks/Dispersal-Networks/nnmatas.txt", sep="\t")
+detach(martcoord)
+
 ####**Step 1. **Open Martinazo and Matasgordas matrix
 
 #Martinazo
@@ -12,6 +25,8 @@ matas<-read.csv("~/Desktop/Palmito/Postdoc-Interaction Networks/matas_visit.csv"
 row.names(matas)
 ####**Step 2.** Using "bipartite" package
 
+library(biparite)
+
 #####*Step 2.1.* Ploting the web.
 
 par(mfrow = c(2,1))
@@ -20,19 +35,22 @@ plotweb(matas, col.low=c(gray(seq(0.1,1,length=42))), col.high=c("white","white"
 
 ####**Step 3.** Calculate indices describing network topography. 
 
+##**Step 3.** Calculate different indexes of a network.
+
 #Centrality measures
 
-#Degree
-specieslevel(mart,index="normalised degree")
-specieslevel(matas,index="normalised degree")
+#Degree, Betweeness and Closeness
 
-#Closeness
-specieslevel(mart,index="closeness")
-specieslevel(matas,index="closeness")
+HLspplvlmart<-specieslevel(mart)
+HLspplvlmart$`higher level`[,c(2,11,12,13,14)]
+LLspplvlmart<-specieslevel(mart)
+LLspplvlmart$`lower level`[,c(2,11,12,13,14)]
 
-#Betweeness
-specieslevel(mart,index="betweenness")
-specieslevel(matas,index="betweenness")
+spplvlmatas<-specieslevel(matas)
+spplvlmatas$`higher level`[,c(2,11,12,13,14)]
+LLspplvlmatas<-specieslevel(matas)
+LLspplvlmatas$`lower level`[,c(2,11,12,13,14)]
+
 
 
 ##Networks analysis. Network topology indexes
@@ -56,5 +74,209 @@ write.table(clos_mart, "/Users/apple/Desktop/Palmito/Postdoc-Interaction Network
 write.table(clos_mart, "/Users/apple/Desktop/Palmito/Postdoc-Interaction Networks/Dispersal Networks/clos_mart.txt", sep="\t")
 write.table(clos_mart, "/Users/apple/Desktop/Palmito/Postdoc-Interaction Networks/Dispersal Networks/clos_mart.txt", sep="\t")
 write.table(clos_mart, "/Users/apple/Desktop/Palmito/Postdoc-Interaction Networks/Dispersal Networks/clos_mart.txt", sep="\t")
+
+#Full data of visits
+
+par(mfrow=c(1,1))
+mart_matas_visits<-read.csv("~/Desktop/Palmito/Postdoc-Interaction Networks/mart_matas_visits.csv",header=TRUE,check.names=FALSE,row.names=1)
+plotweb(mart_matas_visits, col.low=c(gray(seq(0.1,1,length=63))),col.high=c("white","white","gray30","gray30","black","black","darkgray"),method="normal", text.rot=90,low.lablength=10, high.lablength=8)
+
+#Degree, Betweeness and Closeness
+
+HLspplvlmartmatasvisits<-specieslevel(mart_matas_visits)
+martmatasHL<-HLspplvlmartmatasvisits$`higher level`[,c(2,11,12,13,14)]
+LLspplvlmartmatasvisits<-specieslevel(mart_matas_visits)
+martmatasLL<-LLspplvlmartmatasvisits$`lower level`[,c(2,11,12,13,14)]
+
+write.csv(martmatasHL, file = "martmatasHL.csv")
+write.csv(martmatasLL, file = "martmatasLL.csv")
+
+#Centrality related to aggregation or nearest neighbour analysis
+
+
+library(ISLR)
+#Martinazo
+mart_wv<-read.csv("~/Desktop/Palmito/Postdoc-Interaction Networks/mart_wv.csv",header=TRUE)
+mart_wv
+mart_v<-glm(nd~nn+ x+ y, data = mart_wv)
+par(mfrow=c(3,3))
+plot(mart_v)
+summary(mart_v)
+anova(mart_v)
+
+
+#Estimate Std. Error t value Pr(>|t|)    
+#(Intercept) 0.347703   0.083478   4.165 0.000563 ***
+ # dens        0.001372   0.030349   0.045 0.964427    
+#  edf Ref.df     F p-value
+#s(nn)       1.000  1.000 0.585   0.454
+#s(del.area) 2.688  3.311 1.061   0.373
+#R-sq.(adj) =  0.0148   Deviance explained = 21.6%
+mart_vnd<-gam(nd~s(nn)+s(del.area)+dens,data = mart_wv)
+plot(mart_vnd,residuals=TRUE, se=TRUE, shade=TRUE,all.terms = TRUE, main = "Normalised degree")
+summary(mart_vnd)
+
+#Estimate Std. Error t value Pr(>|t|)
+#(Intercept)  0.07088    0.05740   1.235    0.232
+#dens        -0.01150    0.02059  -0.558    0.583
+# edf Ref.df     F p-value
+#s(nn)       1.614  1.965 0.929   0.364
+#s(del.area) 1.000  1.000 0.185   0.672
+#R-sq.(adj) =  -0.0313   Deviance explained = 13.1%
+mart_vwb<-gam(wbtw~s(nn)+s(del.area)+dens,data = mart_wv)
+plot(mart_vwb,residuals= TRUE, se=TRUE, shade=TRUE,all.terms = TRUE, main = "Weighted Betweeness")
+summary(mart_vwb)
+
+#Estimate Std. Error t value Pr(>|t|)
+#(Intercept)  0.07088    0.05740   1.235    0.232
+#dens        -0.01150    0.02059  -0.558    0.583
+# edf Ref.df     F p-value
+#s(nn)       1.614  1.965 0.929   0.364
+#s(del.area) 1.000  1.000 0.185   0.672
+#R-sq.(adj) =  -0.0313   Deviance explained = 13.1%
+mart_vwc<-gam(wcls~s(nn)+s(del.area)+dens,data = mart_wv)
+plot(mart_vwc,residuals= TRUE, se=TRUE, shade=TRUE,all.terms = TRUE, main = "Weighted Closeness")
+summary(mart_vwc)
+
+#Matasgordas
+matas_wv<-read.csv("~/Desktop/Palmito/Postdoc-Interaction Networks/matas_wv.csv",header=TRUE)
+matas_wv
+
+#Estimate Std. Error t value Pr(>|t|)    
+#(Intercept)  0.358628   0.043767   8.194  7.5e-10 ***
+ # dens        -0.006808   0.007391  -0.921    0.363    
+#  edf Ref.df     F p-value  
+#s(nn)       1.157  1.298 0.057  0.8089  
+#s(del.area) 1.674  2.054 3.161  0.0514 .
+#R-sq.(adj) =  0.177   Deviance explained = 25.4%
+matas_vnd<-gam(nd~s(nn)+s(del.area)+dens,data = matas_wv)
+plot(matas_vnd,residuals=TRUE, se=TRUE, shade=TRUE,all.terms = TRUE, main = "Normalised Degree")
+summary(matas_vnd)
+
+#Estimate Std. Error t value Pr(>|t|)  
+#(Intercept)  0.036681   0.020425   1.796   0.0817 .
+#dens        -0.002469   0.003560  -0.693   0.4929  
+#  edf Ref.df     F p-value   
+#s(nn)       1.441  1.748 4.391 0.06918 . 
+#s(del.area) 5.804  6.520 4.601 0.00147 **
+#R-sq.(adj) =   0.42   Deviance explained = 53.7%
+matas_vwb<-gam(wbtw~s(nn)+s(del.area)+dens,data = matas_wv)
+plot(matas_vwb,residuals= TRUE, se=TRUE, shade=TRUE,all.terms = TRUE, main = "Weighted Betweeness")
+summary(matas_vwb)
+
+#Estimate Std. Error t value Pr(>|t|)    
+#(Intercept)  0.0267349  0.0032962   8.111 8.16e-10 ***
+ # dens        -0.0008312  0.0005509  -1.509     0.14    
+#  edf Ref.df     F p-value
+#s(nn)         1      1 0.013   0.910
+#s(del.area)   1      1 0.014   0.906
+#R-sq.(adj) =  0.0151   Deviance explained = 8.72%
+matas_vwc<-gam(wcls~s(nn)+s(del.area)+dens,data = matas_wv)
+plot(matas_vwc,residuals= TRUE, se=TRUE, shade=TRUE,all.terms = TRUE, main = "Weighted Closeness")
+summary(matas_vwc)
+
+
+#Fulldata with values of centrality pasting the matrix analyzed separetly
+
+mart_matas_wv<-read.csv("~/Desktop/Palmito/Postdoc-Interaction Networks/mart_matas_wv.csv",header=TRUE)
+mart_matas_wv
+
+#Estimate Std. Error t value Pr(>|t|)    
+#(Intercept)  0.362154   0.036258   9.988 2.79e-14 ***
+ # dens        -0.006793   0.007441  -0.913    0.365    
+#  edf Ref.df     F p-value
+#s(nn)       1.474  1.800 0.287   0.663
+#s(del.area) 3.726  4.474 0.784   0.464
+#R-sq.(adj) =  0.0798   Deviance explained = 16.8%
+mart_matas_vnd<-gam(nd~s(nn)+s(del.area)+dens,data = mart_matas_wv)
+plot(mart_matas_vnd,residuals=TRUE, se=TRUE, shade=TRUE, all.terms = TRUE, main = "Normalised Degree")
+summary(mart_matas_vnd)
+plot(mart_matas_wv$nd,mart_matas_wv$nn)
+
+#Estimate Std. Error t value Pr(>|t|)   
+#(Intercept)  0.056167   0.020745   2.708  0.00882 **
+ # dens        -0.006097   0.004233  -1.440  0.15502   
+#  edf Ref.df     F p-value
+#s(nn)       1.749  2.176 2.233   0.109
+#s(del.area) 2.265  2.831 2.066   0.129
+#R-sq.(adj) =  0.107   Deviance explained = 17.6%
+mart_matas_vwb<-gam(wbtw~s(nn)+s(del.area)+dens,data = mart_matas_wv)
+plot(mart_matas_vwb,residuals= TRUE, se=TRUE, shade=TRUE, all.terms = TRUE, main = "Weighted Betweeness")
+summary(mart_matas_vwb)
+plot(mart_matas_wv$wbtw,mart_matas_wv$nn)
+
+#Estimate Std. Error t value Pr(>|t|)    
+#(Intercept)  0.0366969  0.0043246   8.486 6.28e-12 ***
+ # dens        -0.0017501  0.0008746  -2.001   0.0498 *  
+#  edf Ref.df     F p-value
+#s(nn)       1.784   2.23 1.706   0.222
+#s(del.area) 1.000   1.00 0.358   0.552
+#R-sq.(adj) =  0.125   Deviance explained = 17.6%
+mart_matas_vwc<-gam(wcls~s(nn)+s(del.area)+dens,data = mart_matas_wv)
+plot(mart_matas_vwc,residuals= TRUE, se=TRUE, shade=TRUE, all.terms=TRUE, main = "Weighted Closeness")
+summary(mart_matas_vwc)
+plot(mart_matas_wv$wcls,mart_matas_wv$nn)
+
+
+#Fulldata with values of centrality using the matrix analyzed together
+
+mart_matas_wv1<-read.csv("~/Desktop/Palmito/Postdoc-Interaction Networks/Dispersal-Networks/martmatasLL.csv",header=TRUE)
+mart_matas_wv1
+
+#            Estimate Std. Error t value Pr(>|t|)    
+#(Intercept)  0.365951   0.035586  10.283 1.21e-14 ***
+#  dens        -0.010590   0.007736  -1.369    0.176    
+#  edf Ref.df     F p-value
+#s(nn)       2.505  3.116 0.476   0.671
+#s(del.area) 1.000  1.000 0.454   0.503
+#R-sq.(adj) =  0.0516   Deviance explained =   12%
+mart_matas_vnd1<-gam(nd~s(nn)+s(del.area)+dens,data = mart_matas_wv1)
+plot(mart_matas_vnd1,residuals=TRUE, se=TRUE, shade=TRUE, all.terms = TRUE, main = "Normalised Degree")
+summary(mart_matas_vnd1)
+plot(mart_matas_wv1$nd,mart_matas_wv1$nn)
+
+
+#            Estimate Std. Error t value Pr(>|t|)
+#(Intercept)  0.035548   0.022063   1.611    0.113
+#dens        -0.004998   0.004709  -1.061    0.293
+# edf Ref.df     F p-value
+#s(nn)       1.463  1.789 1.227   0.243
+#s(del.area) 1.441  1.767 0.171   0.775
+#R-sq.(adj) =  -0.00119   Deviance explained = 6.18%
+#GCV = 0.0097787  Scale est. = 0.0090175  n = 63
+mart_matas_vwb1<-gam(wbtw~s(nn)+s(del.area)+dens,data = mart_matas_wv1)
+plot(mart_matas_vwb1,residuals= TRUE, se=TRUE, shade=TRUE,all.terms = TRUE, main = "Weighted Betweeness")
+summary(mart_matas_vwb1)
+plot(mart_matas_wv1$wbtw,mart_matas_wv1$nn)
+
+
+#Estimate Std. Error t value Pr(>|t|)    
+#(Intercept)  0.0186029  0.0022797    8.16  3.1e-11 ***
+ # dens        -0.0008436  0.0004847   -1.74   0.0871 .  
+#  edf Ref.df     F p-value
+#s(nn)       1.482   1.82 1.428   0.342
+#s(del.area) 1.000   1.00 0.193   0.662
+#R-sq.(adj) =  0.118   Deviance explained = 16.8%
+#GCV = 0.00010552  Scale est. = 9.8013e-05  n = 63
+mart_matas_vwc1<-gam(wcls~s(nn)+s(del.area)+dens,data = mart_matas_wv1)
+plot(mart_matas_vwc1,residuals= TRUE, se=TRUE, shade=TRUE, all.terms=TRUE, main = "Weighted Closeness")
+summary(mart_matas_vwc1)
+plot(mart_matas_wv1$wcls,mart_matas_wv1$nn)
+
+
+#Interaction motifs
+
+#Obtaining Median of non-zero interactions. 
+
+##Creating a matrix with 0=NA
+martmat_vNA<-mart_matas_visits
+martmat_vNA[martmat_vNA==0]<-NA
+martmat_vNA
+
+##Median excluding NAs(e.g. 0). "The median for proportional visits is (0.1667)". All datas above this value will mean strong interaction and all below weak interaction
+median(as.matrix(martmat_vNA),na.rm=TRUE)
+
+
+
 
 
